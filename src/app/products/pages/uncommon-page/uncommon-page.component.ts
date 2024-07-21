df43f57c5fb127ad0cde270452b5cb064be61559
Santiago Ortiz Cardona <sanortizca@gmail.com>
2024-07-21 12:39:51 -0500

    Se usa el pipe de Angular i18nPlural que permite cambiaro mostrar una frase de acuerdo a un valor. Este pipe se encuentra en @angular/common y recibe varios arguemntos. Además se realiza el tipeo de los diferentes propiedades que usan {[key:string]:string}, es decir tienen propiedades de tipo string y el valor de essas propiedades también es un string

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -10,13 +10,37 @@ export class UncommonPageComponent {
   public name: string = 'Fernando';
   public gender: 'male' | 'female' = 'male';
   /* InvitationMap es un objeto literal */
-  public invitationMap = {
-    'male' : 'invitarlo',
-    'female': 'invitarla',
-  }
+  public invitationMap: {[key:string]:string} = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
 
   public changeClient(): void {
     this.name = 'Melissa';
     this.gender = 'female';
+  }
+
+  //i18nPlural
+  public clients: string[] = [
+    'Maria',
+    'Pedro',
+    'Fernando',
+    'Hernando',
+    'Eduardo',
+    'Melissa',
+    'Natalia',
+  ];
+  /* Dependiendo del valor al que sea igual se coloca una frase  */
+
+  /* La propiedad clientsMap es un objeto cuyas claves son de tipo string y sus valores también son de tipo string. En TypeScript, esto se denota como {[key:string]:string}. */
+  public clientsMap: { [key: string]: string } = {
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos 2 personas esperando',
+    'other': 'tenemos # clientes esperando',
+  };
+
+  public deleteClient(): void {
+    this.clients.shift();
   }
 }
