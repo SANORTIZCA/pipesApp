368beec953b29ec8997e78ee6e54792bf9ac8ea2
Santiago Ortiz Cardona <sanortizca@gmail.com>
2024-07-26 11:04:19 -0500

    Se agregan más argumentos a nuestro pipe personalizado toggleCasePipe lo que permite cambiar de acuerdo a ciertas condiciones. Además usamos el ToolBar de PrimeNG.

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'products-order',
   templateUrl: './order.component.html',
-  styles: ``
+  styles: ``,
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
+  public items: MenuItem[] = [];
 
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Update',
+        icon: 'pi pi-refresh',
+      },
+      {
+        label: 'Delete',
+        icon: 'pi pi-times',
+      },
+    ];
+  }
 }
--- src/app/products/pipes/toggle-case.pipe.ts
+++ src/app/products/pipes/toggle-case.pipe.ts
@@ -5,7 +5,11 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 
 export class ToggleCasePipe implements PipeTransform {
-  transform(value: string):string {
-    return value.toUpperCase();
+  /* Operador rest ...args, según la documentación para los demás arguemntos del pipe */
+  /* Lo recomendado son 3 argumentos, si son más de 3 lo mejor es recibir un objeto */
+  transform(value: string, toUpper:boolean = false):string {
+    return (toUpper)
+    ? value.toUpperCase()
+    : value.toLowerCase();
   }
 }
