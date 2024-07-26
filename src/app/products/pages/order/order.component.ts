a81bc4a13263d563cde00f69f561ec662c1e78ad
Santiago Ortiz Cardona <sanortizca@gmail.com>
2024-07-26 11:06:22 -0500

    Se elimina el implements OnInit de orderComponents.ts porque no es necesario para el funcionamiento normal de la app.

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -1,24 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'products-order',
   templateUrl: './order.component.html',
   styles: ``,
 })
-export class OrderComponent implements OnInit {
-  public items: MenuItem[] = [];
+export class OrderComponent {
 
-  ngOnInit() {
-    this.items = [
-      {
-        label: 'Update',
-        icon: 'pi pi-refresh',
-      },
-      {
-        label: 'Delete',
-        icon: 'pi pi-times',
-      },
-    ];
-  }
 }
